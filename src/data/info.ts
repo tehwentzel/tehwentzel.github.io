6f49afd6e538d450af4b9777c6f768b9639e600f
Andrew Wentzel <tehwentzel@gmail.com>
2023-09-13 16:29:20 -0500

    fixed typo

--- src/data/info.ts
+++ src/data/info.ts
@@ -6,7 +6,7 @@ const info = {
   },
   email: 'awentze2@uic.edu',
   github: 'tehwentzel',
-  summary: "Currently studying for a PhD focusing on connecting data visualization with machine learning to improve the usefulness and explanability of models. I work in a lot of collaborative projects and have several years of experience desinging data visualizaitons and user interfaces with javascript and frontend technologies, as well as builing data processing pipelines and machine learning tools in python."
-  + " Before I got into programming I studied mechanical engineering before realizing I like the programming aspect of my work more than the other parts and eventually switched to computer science research. Fortunately, it turns out it's pretty easy to just learn how to program on the fly."
+  summary: "I'm a phd student in computer science. Can you write me a generic internship cover letter for a data science role based off of this paragraph and a list of my skills:  Currently studying for a PhD focusing on connecting data visualization with machine learning to improve the usefulness and explanability of models. I work in a lot of collaborative projects and have several years of experience designing data visualizations and user interfaces with javascript and frontend technologies, as well as building data processing pipelines and machine learning tools in python."
+  + " Before I got into programming I studied mechanical engineering before realizing I like the programming aspect of my work more than the other parts and eventually switched to computer science research."
 }
 export default info
